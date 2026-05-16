0da461087f1f198565e23e93b32e00252671c921
BrandonLuybenCA <Brandonluyben@icloud.com>
2026-05-16 19:03:11 -0400

    Update VeilApp.swift

--- VeilApp.swift
+++ VeilApp.swift
@@ -1,23 +1,10 @@
 import SwiftUI
-import FirebaseCore
 
 @main
 struct VeilApp: App {
-    @StateObject private var authManager = FirebaseManager()
-
-    init() {
-        FirebaseApp.configure()
-    }
-
     var body: some Scene {
         WindowGroup {
-            if authManager.isAuthenticated {
-                ContentView()
-                    .environmentObject(authManager)
-            } else {
-                LoginView()
-                    .environmentObject(authManager)
-            }
+            ContentView()
         }
     }
 }
