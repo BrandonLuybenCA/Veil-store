5cb316cc082396089d5a7113e368dcb7ebbbcf2e
askitdomains <luybenbrandon35@gmail.com>
2026-05-15 21:32:46 -0400

    Create AppEntry.swift

---
+++ Models/AppEntry.swift
@@ -0,0 +1,16 @@
+import FirebaseFirestoreSwift
+
+struct AppEntry: Identifiable, Codable {
+    @DocumentID var id: String?
+    var title: String
+    var subtitle: String
+    var description: String
+    var iconURL: String
+    var screenshots: [String]
+    var category: String
+    var manifestURL: String
+    var version: String
+    var sizeMB: Double
+    var featured: Bool
+    var isActive: Bool
+}
